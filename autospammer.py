a664c834bd8ae1393d95f2b69c311b2405bb3377
Abstract <21150449+abstractpotato@users.noreply.github.com>
2026-09-24 13:52:29 -0400

    Sep 24, 2026 - 01:52 PM

---
+++ autospammer.py
@@ -0,0 +1,32 @@
+import pyautogui, time, random
+import lmstudio as lms
+
+model = lms.llm("llama-3.1-8b-lexi-uncensored-v2")
+
+prompt_count = int(input("how many prompts? "))
+prompts = []
+for i in range(prompt_count):
+    prompt = input(f"what nonsense to generate? {i + 1}: ")
+    prompts.append(f"Write a random fact about {prompt}. Must be made up. Make it a single 3 - 5 word sentance.")
+    
+count = int(input("count: "))
+sentances = []
+
+for i in range(count):
+    print(f"- generating: {i+1}/{count}")
+    request = random.choice(prompts)
+    result = str(model.respond(request))
+    print(result)
+    sentances.append(result)
+        
+input("ready? [press enter]")
+print("sleeping... 10s")
+for i in range(10):
+    print(i + 1)
+    time.sleep(1)
+    
+for x in sentances:
+    pyautogui.write(x, interval=0.01)
+    pyautogui.press("enter")
+    delay = random.random() + random.randint(2, 6)
+    time.sleep(delay)
